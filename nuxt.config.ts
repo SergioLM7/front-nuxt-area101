3a6a71ec88ab2feda67cc8111f332fd6ffcb251a
SergioLM7 <sergiolillom@gmail.com>
2024-10-16 19:21:57 +0200

    .env added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
+      title: 'The Books Project',
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
       ]
